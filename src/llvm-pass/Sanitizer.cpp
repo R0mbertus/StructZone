7275e71a91918c8f9c8b81e26bd1528cb1b8728c
Jessasyn <43725856+Jessasyn@users.noreply.github.com>
2025-06-03 11:59:34 +0200

    more work on the sanitizer
    cleaning up the types that store metadata, and a rough plan on how to detect struct access

--- src/llvm-pass/Sanitizer.cpp
+++ src/llvm-pass/Sanitizer.cpp
@@ -9,25 +9,23 @@ using namespace llvm;
 
 namespace {
 	// Forward declaration to be able to use it in field info.
-	class StructInfo;
+	struct StructInfo;
 	
-	class FieldInfo {
-	public:
+	struct FieldInfo {
 		// If the field this info represents is a struct type, will contain the StructInfo for this.
 		// Otherwise, will be NULL.
-		std::shared_ptr<StructInfo> StructType;
+		std::shared_ptr<StructInfo> structInfo;
 		// The size (in bytes) of the field.
 		// In case of a struct, this is usually slightly more than the actual struct size due to alignment.
-		int size;
+		long unsigned int size;
 	};
-    class StructInfo {
-    public:
-    	// The name of the struct.
-    	std::string name;
+	struct StructInfo {
+    	// The llvm struct type.
+    	StructType* type;
     	// The fields present in the struct.
     	std::vector<FieldInfo> fields;
     	// The total size of the struct. Usually slightly more than the summation of the sizes of all fields due to alignment.
-    	int size;
+    	long unsigned int size;
     };
     
 	struct StructZoneSanitizer : PassInfoMixin<StructZoneSanitizer> {
@@ -38,23 +36,21 @@ namespace {
 			std::vector<FieldInfo> fields;
 			for (auto fieldType : s->elements())
 			{
-				FieldInfo field;
-				field.size = dl.getTypeAllocSize(fieldType);
+				FieldInfo field = {
+					nullptr,
+					dl.getTypeAllocSize(fieldType),
+				};
 				if (auto* structType = dyn_cast<StructType>(fieldType))
 				{
-					field.StructType = WalkStruct(structType, dl);
-				}
-				else {
-					field.StructType = NULL;
+					field.structInfo = WalkStruct(structType, dl);
 				}
 				fields.push_back(field);
 			}
-			class StructInfo si = {
-				s->getName().str(),
-				fields
+			struct StructInfo si = {
+				s,
+				fields,
+				dl.getTypeAllocSize(s),
 			};
-			// To avoid a warning on implicit narrowing.
-			si.size = dl.getTypeAllocSize(s);
 			return std::make_shared<StructInfo>(si);
 		}
 	
@@ -70,15 +66,15 @@ namespace {
 			for (auto &structInfo: structs)
 			{
 				int fieldCount = 0;
-				llvm::outs() << "Struct with name " << structInfo->name << " has fields:\n";
+				llvm::outs() << "Struct with name " << structInfo->type->getName() << " has fields:\n";
 				for (auto& fieldInfo: structInfo->fields)
 				{
-					if (fieldInfo.StructType)
+					if (fieldInfo.structInfo)
 					{
 						int inner_fieldCount = 0;
 						llvm::outs() << "\tNested struct type " << fieldCount << ":" << fieldInfo.size << "\n";
-						llvm::outs() << "\t\tStruct with name " << fieldInfo.StructType->name << " has fields:\n";
-						for (auto& inner_fieldInfo: fieldInfo.StructType->fields)
+						llvm::outs() << "\t\tStruct with name " << fieldInfo.structInfo->type->getName() << " has fields:\n";
+						for (auto& inner_fieldInfo: fieldInfo.structInfo->fields)
 						{
 							llvm::outs() << "\t\t\t" << inner_fieldCount << ":" << inner_fieldInfo.size << "\n";
 							inner_fieldCount += 1;
@@ -91,9 +87,27 @@ namespace {
 					fieldCount += 1;
 				}
 			}
+			for (auto &func : M) {
+				// Then it is an external function, and must be linked.
+				// We can't instrument this - though it is probably interesting in a later stage for inflating/deflating structs.
+				if (func.isDeclaration())
+				{
+					continue;
+				}
+				// for now, track:
+				// - alloca's with a known struct type.
+				// - getelementptr whose source is a known struct instance (i.e. an alloca)
+				// that gives all accesses to the struct.
+				// we can then add the redzones in between each pair of fields and change the offsets.
+				// NOTE: how big do the redzones need to be?
+				// NOTE 2: what redzone type do we want to use? i.e., what way do we check if one is hit?
+				llvm::outs() << "function with name " << func.getName() << "\n";
+			}
+
 		    // TODO: from within the pass:
 			// 3. investigate what instructions are practically used to access the structs.
 			// 4. add sanitation checks there, for _internal overflow_.
+			// (i.e., whenever a load happens whose source is a getelementptr associated to a struct instance, first insert a call to a function which crashes if the memory is a redzone)
 			// 5. verify the program now crashes on internal overflow, but not on the safe variant.
 			// 6. move on to external overflows.
 		  return PreservedAnalyses::all();
