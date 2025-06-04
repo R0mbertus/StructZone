851d23ed0905c3f5796e8a59a9af4da5d15ceb10
Robert Klink <roberthklink@gmail.com>
2025-06-05 01:52:14 +0200

    scripts: adapt to `make`

--- test/compile-test.sh
+++ test/compile-test.sh
@@ -1,19 +1,24 @@
 #! /bin/bash
 
-pass_dir="../llvm-pass/bin/"
-# Check if the sanitizer has already been built, build if not. Yes, I'm aware, you could use makefiles for this but i can't be arsed right now.
-if [ ! -e $pass_dir"Sanitizer.so" ]
+set -e
+
+if [ -z "$1" ]
 then
-    dir=$(pwd)
-    cd $pass_dir
-    source compile_pass.sh
-    cd $dir
+    echo "Usage: $0 <filename without extension>"
+    exit 1
+fi
+
+pass_dir="../llvm-pass"
+# Check if the sanitizer has already been built, build if not. Yes, I'm aware, you could use makefiles for this but i can't be arsed right now.
+if [ ! -e $pass_dir"/bin/Sanitizer.so" ]
+then
+    make -C $pass_dir
 fi
 # Then get the llvm IR out of the requested input file.
 clang -S -emit-llvm "$1.c"
 
 # Run the optimization pass.
-opt -load-pass-plugin=$pass_dir"Sanitizer.so" -S -passes=structzone-sanitizer $1".ll" -o $1".out.ll"
+opt -load-pass-plugin=$pass_dir"/bin/Sanitizer.so" -S -passes=structzone-sanitizer $1".ll" -o $1".out.ll"
 # Since this is for testing purposes, we aren't going to remove the IR files.
 
 # And then compile that llvm IR with the sanitation pass enabled.
