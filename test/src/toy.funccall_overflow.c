c6e116230a0b6204b6e7753809ec35f8cf8a4162
JoachimBose <joachimbose48@gmail.com>
2025-06-17 16:15:03 +0200

    Made: function parameter tests

---
+++ test/src/toy.funccall_overflow.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+struct Simple {
+    int zero;
+    char one[2];
+    char two[3];
+    char three;
+};
+
+void func(struct Simple* example){
+    for (int i = -4; i < 5; i++)
+    {
+        example->one[i] = 0;
+    }
+    // Print to see what the contents are;
+    printf("zero %i\n", example->zero);
+    for (int i = 0; i < 2; i++)
+    {
+        printf("one %i %i\n", i, example->one[i]);
+    }
+    for (int i = 0; i < 3; i++)
+    {
+        printf("two %i %i\n", i, example->two[i]);
+    }
+    printf("three %i\n", example->three);
+}
+
+int main() {
+    // Note: we very explicitly do not use struct initializer, as they get lowered to memset / memcpy.
+    struct Simple* example = malloc(sizeof(struct Simple));
+    example->zero = 7;
+    example->one[0] = 1;
+    example->one[1] = 2;
+    example->two[0] = 3;
+    example->two[1] = 4;
+    example->two[2] = 5;
+    example->three = 6;
+     // This overflows the first field (which has a length of two), thereby also setting the 'two' array.
+    // Because it starts at -4, it also overwrites the zero field, which is an int32 (thus 4 bytes long)
+    // But it shouldn't touch the 'three' char.
+    func(example);
+    free(example);
+    return 0;
+}
